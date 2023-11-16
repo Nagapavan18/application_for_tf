465692b6e1f790760f79bfb2119cfee48688b3f2
Nagapavan18 <123530794+Nagapavan18@users.noreply.github.com>
2023-11-16 16:50:56 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 
 # Get the IDs of running containers publishing port 8000
 running_containers=$(sudo docker ps -q --filter "publish=8000")
@@ -12,3 +13,5 @@ while IFS= read -r container_id; do
     echo "Stopping container $container_id..."
     sudo docker stop "$container_id"
 done <<< "$running_containers"
+
+
