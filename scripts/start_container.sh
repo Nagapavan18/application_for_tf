fc42fa579cc2106fa353441bdb2d9aa816cf51fc
Nagapavan18 <123530794+Nagapavan18@users.noreply.github.com>
2023-11-17 17:38:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Authenticate Docker with ECR
-/usr/bin/aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/g2a3v3g3
+/usr/bin/aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/h3k2m2f5
 # Pull the Docker image from ECR
-/usr/bin/docker pull public.ecr.aws/g2a3v3g3/python/flask/repo:latest
+/usr/bin/docker pull public.ecr.aws/h3k2m2f5/pavan_repo:latest
 # Run the Docker image as a containers
-/usr/bin/docker run -d -p 8000:5000 public.ecr.aws/g2a3v3g3/python/flask/repo:latest
+/usr/bin/docker run -d -p 8000:5000 public.ecr.aws/h3k2m2f5/pavan_repo:latest
